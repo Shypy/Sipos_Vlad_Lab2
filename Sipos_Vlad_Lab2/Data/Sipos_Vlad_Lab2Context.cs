334cc80d4dd90d1a17f6ce427fdd9bc58fc332a9
shypy <vlad.sipos@ymail.com>
2024-11-17 11:17:59 +0200

    save before removing index

--- Sipos_Vlad_Lab2/Data/Sipos_Vlad_Lab2Context.cs
+++ Sipos_Vlad_Lab2/Data/Sipos_Vlad_Lab2Context.cs
@@ -17,5 +17,6 @@ namespace Sipos_Vlad_Lab2.Data
         public DbSet<Sipos_Vlad_Lab2.Models.Book> Book { get; set; } = default!;
         public DbSet<Sipos_Vlad_Lab2.Models.Publisher> Publisher { get; set; } = default!;
         public DbSet<Sipos_Vlad_Lab2.Models.Author> Author { get; set; } = default!;
+        public DbSet<Sipos_Vlad_Lab2.Models.Category> Category { get; set; } = default!;
     }
 }
--- Sipos_Vlad_Lab2/Models/Book.cs
+++ Sipos_Vlad_Lab2/Models/Book.cs
@@ -18,6 +18,8 @@ namespace Sipos_Vlad_Lab2.Models
         public DateTime PublishingDate { get; set; }
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; } //navigation property
+        
+        public ICollection<BookCategory>? BookCategories { get; set; }
 
     }
 }
