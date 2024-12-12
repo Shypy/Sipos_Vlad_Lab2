7f4b45550c60e184c369bdb945ec9be7b881996c
shypy <vlad.sipos@ymail.com>
2024-12-12 11:59:40 +0200

    Laborator6

--- Sipos_Vlad_Lab2/Pages/Books/Delete.cshtml.cs
+++ Sipos_Vlad_Lab2/Pages/Books/Delete.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Sipos_Vlad_Lab2.Models;
 
 namespace Sipos_Vlad_Lab2.Pages.Books
 {
+    [Authorize(Roles = "Admin")]
     public class DeleteModel : PageModel
     {
         private readonly Sipos_Vlad_Lab2.Data.Sipos_Vlad_Lab2Context _context;
